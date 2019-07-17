93b6b4f1d5a5676f666355b52e07a59c26974261
Erik Maide <maide.erik@gmail.com>
2019-07-17 22:31:26 +0300

    zxll

--- server.js
+++ server.js
@@ -23,13 +23,14 @@ const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
-app.get('/', (req, res) => {"This is working"})
+app.get('/', (req, res) => {res.send("This is working")})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`app is running on port ${process.env.PORT}´)}
+app.listen(process.env.PORT || 3000, ()=>
+
+  {console.log(`app is running on port ${process.env.PORT}´); }
 )
