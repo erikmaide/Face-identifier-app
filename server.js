978ed3e42b3bc107e093a7ff76774a9158b8d92d
Erik Maide <maide.erik@gmail.com>
2019-07-17 22:38:05 +0300

    testing

--- server.js
+++ server.js
@@ -30,7 +30,5 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=>
-
-  {console.log(`app is running on port ${process.env.PORT}´); }
+app.listen(process.env.PORT || 3000, console.log("It is working");
 )
